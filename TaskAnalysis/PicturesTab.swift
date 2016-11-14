09886d40ee4556208787a7ebeb32e0ed5172976a
Greg <gettingssc@aim.com>
2016-11-14 11:42:11 -0500

    Trying to pull

---
+++ TaskAnalysis/PicturesTab.swift
@@ -0,0 +1,25 @@
+//
+//  PicturesTab.swift
+//  TaskAnalysis
+//
+//  Created by Greg Gettings on 11/14/16.
+//  Copyright © 2016 Greg Gettings. All rights reserved.
+//
+
+import UIKit
+
+class PicturesTab: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
