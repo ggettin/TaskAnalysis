ac8ba683105cb4a0da20c14e9b4904d91b0f621b
Greg <gettingssc@aim.com>
2016-11-13 20:24:16 -0500

    There's now a collection view. With some hardcoded tasks/images

---
+++ TaskAnalysis/CustomCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomCollectionViewCell.swift
+//  TaskAnalysis
+//
+//  Created by Greg Gettings on 11/13/16.
+//  Copyright © 2016 Greg Gettings. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var taskName: UILabel!
+    
+    @IBOutlet var taskImage: UIImageView!
+    
+    @IBOutlet var completionImage: UIImageView!
+    
+
+
+}
--- TaskAnalysis/ViewController.swift
+++ TaskAnalysis/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
 
+    @IBOutlet var collectionView: UICollectionView!
+    
+    let taskTitles = ["Sweeping", "Laundry", "Fold Napkins", "Clean Dishes", "Cook Pasta"]
+    
+    let taskImages = [UIImage(named: "sweeping"), UIImage(named: "laundry"), UIImage(named: "foldNapkins"), UIImage(named: "cleanDishes"), UIImage(named: "cookPasta")]
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.taskTitles.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CustomCollectionViewCell
+        
+        cell.taskName.text = self.taskTitles[indexPath.row]
+        cell.taskImage.image = self.taskImages[indexPath.row]
+        cell.completionImage.image = UIImage(named: "completed")
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
