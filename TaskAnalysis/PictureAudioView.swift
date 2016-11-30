3c4cc515ab6ca7befcca4f3fb335b2f4a7596606
jtmarro <jordanmarro@42.211.21.198.tigernet.wifi.dyn.clemson.edu>
2016-11-30 14:39:04 -0500

    changed audio back

--- TaskAnalysis/PictureAudioView.swift
+++ TaskAnalysis/PictureAudioView.swift
@@ -38,16 +38,16 @@ class PictureAudioView: UIViewController, UITabBarDelegate {
         let minutes = UInt8(currentTime / 60)
         let seconds = UInt8(currentTime)
         let endMin = UInt8(endTime / 60)
-        //let endSec = UInt8(endTime)
+        let endSec = UInt8(endTime)
         
         let strMin = String(format: "%02d", minutes)
         let strSec = String(format: "%02d", seconds)
         let strEndMin = String(format: "%02d", endMin)
-        //let strEndSec = String(format: "%02d", endSec)
+        let strEndSec = String(format: "%02d", endSec)
 
         //update labels
         timerStart.text = "\(strMin):\(strSec)"
-       // timerEnd.text = "\(strEndMin):\(strEndSec)"
+       timerEnd.text = "\(strEndMin):\(strEndSec)"
 
     }
     
