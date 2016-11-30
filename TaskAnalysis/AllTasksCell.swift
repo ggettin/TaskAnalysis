089d0fa680d2b451b32f9c8d737e2ca11c15aede
jtmarro <jordanmarro@42.211.21.198.tigernet.wifi.dyn.clemson.edu>
2016-11-30 10:31:22 -0500

    added all tasks controller

---
+++ TaskAnalysis/AllTasksCell.swift
@@ -0,0 +1,32 @@
+//
+//  AllTasksCell.swift
+//  TaskAnalysis
+//
+//  Created by Jordan Marro on 11/29/16.
+//  Copyright © 2016 Greg Gettings. All rights reserved.
+//
+
+import UIKit
+
+class AllTasksCell: UITableViewCell {
+
+    
+    @IBOutlet var TaskLabel: UILabel!
+    
+    @IBOutlet var TaskImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+    
+    
+}
